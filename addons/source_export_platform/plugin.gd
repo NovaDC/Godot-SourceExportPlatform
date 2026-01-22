2289b3f91e072fbfb67a62a5a9621f59cdcb6052
NovaDC <164206531+NovaDC@users.noreply.github.com>
2026-01-22 13:25:16 -0600

    Assist script documentation loading
    Makes the scripts automatically load in the editor when appropriate, to make the editor documentation viewer more reliable for those scripts.

--- addons/source_export_platform/plugin.gd
+++ addons/source_export_platform/plugin.gd
@@ -8,7 +8,19 @@ const PLUGIN_NAME_INTERNAL := "source_export_platform"
 
 const PLUGIN_ICON_FALLBACK := preload("./icon.svg")
 
+const ENSURE_SCRIPT_DOCS:Array[Script] = [
+	preload("./source_export_platform.gd"),
+]
+
 var _export_platform_ref:SourceEditorExportPlatform = null
+
+# Every once ands a while the script docs simply refuse to update properly.
+# This nudges the docs into a ensuring that the important scripts added by
+# this addon are actually loaded.
+func _ensure_script_docs() -> void:
+	var edit := get_editor_interface().get_script_editor()
+	for scr in ENSURE_SCRIPT_DOCS:
+		edit.update_docs_from_script(scr)
 
 func _get_plugin_name() -> String:
 	return PLUGIN_NAME
@@ -20,10 +32,12 @@ func _get_plugin_icon() -> Texture2D:
 	return ico
 
 func _enter_tree() -> void:
+	_ensure_script_docs()
 	if EditorInterface.is_plugin_enabled(PLUGIN_NAME_INTERNAL):
 		_try_init_platform()
 
 func _enable_plugin() -> void:
+	_ensure_script_docs()
 	_try_init_platform()
 
 func _disable_plugin() -> void:
