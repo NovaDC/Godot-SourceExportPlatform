f4b9bfd7b02d3ab0ac7929f6fcf285109082459d
NovaDC <164206531+NovaDC@users.noreply.github.com>
2026-01-22 00:08:49 -0600

    Improve path handling and resolution

--- addons/source_export_platform/source_export_platform.gd
+++ addons/source_export_platform/source_export_platform.gd
@@ -9,7 +9,54 @@ extends ToolEditorExportPlatform
 ## Usefull for making automatic source code exports.[br]
 ## Requires the NovaTools plugin as a dependency.
 
+func _path_normalize(raw_path:Variant) -> String:
+	var path:String = ""
+	match typeof(raw_path):
+		TYPE_STRING:
+			path = raw_path
+		TYPE_STRING_NAME, TYPE_NODE_PATH:
+			path = str(raw_path)
+		TYPE_PACKED_STRING_ARRAY:
+			path = "/".join(raw_path)
+		TYPE_ARRAY:
+			path = "/".join(raw_path.map(str))
+
+	path = path.strip_escapes().strip_edges()
+
+	if path.is_empty():
+		return ""
+
+	if path.begins_with("file://"):
+		path = path.trim_prefix("file://")
+	elif path.begins_with("res://") or path.begins_with("user://"):
+		path = ProjectSettings.globalize_path(path)
+	elif path.is_relative_path():
+		path = path.trim_prefix("./")
+		path = ProjectSettings.globalize_path("res://").path_join(path)
+	path = path.simplify_path()
+
+	return path
+
+func _has_valid_export_configuration(preset:EditorExportPreset, debug:bool):
+	if debug:
+		push_error("Debug exports not supported for source.")
+
+	var preset_ok = super._has_valid_export_configuration(preset, debug)
+
+	var source_dir := _path_normalize(preset.get_or_env("source_directory", ""))
+	if source_dir.is_empty():
+		add_config_error("Invalid source directory path.")
+		preset_ok = false
+	elif not DirAccess.dir_exists_absolute(source_dir):
+		add_config_error("Non existent source directory.")
+		preset_ok = false
+
+	return preset_ok
+
 func _get_name():
+	return "SourceExport"
+
+func _get_os_name():
 	return "Source"
 
 func _get_logo():
@@ -34,16 +81,44 @@ func _get_export_options():
 		},
 	] + super._get_export_options()
 
-func _has_valid_project_configuration(preset: EditorExportPreset):
-	return true
+func _get_export_option_warning(preset:EditorExportPreset, option:StringName):
+	match (option):
+		"source_directory":
+			var parsed_source_dir := _path_normalize(preset.get_or_env("source_directory", ""))
+			if parsed_source_dir.is_empty():
+				return ""
 
-func _export_hook(preset: EditorExportPreset, path: String):
-	var ret := OK
-	
+			var warning_strings := PackedStringArray()
+			if not parsed_source_dir.begins_with(ProjectSettings.globalize_path("res://").rstrip("/")):
+				warning_strings.append("Source directory not local to project.")
+			if DirAccess.dir_exists_absolute(parsed_source_dir):
+				if (DirAccess.get_directories_at(parsed_source_dir) + DirAccess.get_files_at(parsed_source_dir)).is_empty():
+					warning_strings.append("Source directory empty.")
+			return "\n".join(warning_strings)
+
+		_:
+			return ""
+
+func _export_hook(preset: EditorExportPreset, path:String):
+	var zipped := bool(preset.get_or_env("zipped", ""))
+
+	path = _path_normalize(path)
+	if path.is_empty():
+		return ERR_FILE_BAD_PATH
+	if zipped:
+		if path.ends_with("/"):
+			return ERR_FILE_BAD_PATH
+		if not path.get_file().is_valid_filename():
+			return ERR_FILE_BAD_PATH
+
+	var source_dir:String = _path_normalize(preset.get_or_env("source_directory", ""))
+	if source_dir.is_empty():
+		return ERR_FILE_BAD_PATH
+	if not DirAccess.dir_exists_absolute(source_dir):
+		return ERR_DOES_NOT_EXIST
+
 	var f:Callable
-	
-	var source_dir = preset.get_or_env("source_directory", "").lstrip(".").lstrip("/").rstrip("/")
-	if preset.get_or_env("zipped", ""):
+	if zipped:
 		f = NovaTools.compress_zip_async.bind(source_dir, path)
 	else:
 		f = NovaTools.copy_recursive.bind(source_dir, path)
